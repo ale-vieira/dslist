ce0a6366a3c658f086bfea3dc4ece4f994a7fce2
ale-vieira <alexandrevieira76@gmail.com>
2023-05-09 16:49:30 -0300

    Game seed

--- src/main/java/com/alevieira/dslist/model/Game.java
+++ src/main/java/com/alevieira/dslist/model/Game.java
@@ -16,7 +16,7 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
     private String shortDescription;
@@ -27,12 +27,12 @@ public class Game {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -71,12 +71,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
     public void setPlatform(String platform) {
-        this.platform = platform;
+        this.platforms = platforms;
     }
 
     public Double getScore() {
